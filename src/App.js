187064398fd03b116c5b05c12c8f0e96e17ec828
Nicollas Henrique <nicollass049@gmail.com>
2025-04-06 22:26:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HomePagge from './componetes/homePagge';
+
+export default function App() {
+  return (
+    <div className="App">
+      <HomePagge/>
+    </div>
+  )
+}
